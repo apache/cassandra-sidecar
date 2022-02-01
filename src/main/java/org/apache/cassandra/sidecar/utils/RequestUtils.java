c36fa643eb987f3923ff9429fcef9e61780ae0b5
Saranya Krishnakumar <saranya_k@apple.com>
2022-02-01 10:53:30 -0800

    Add test cases for RequestUtils

--- src/main/java/org/apache/cassandra/sidecar/utils/RequestUtils.java
+++ src/main/java/org/apache/cassandra/sidecar/utils/RequestUtils.java
@@ -7,7 +7,7 @@ public class RequestUtils
 {
     /**
      * Given a combined host address like 127.0.0.1:9042 or [2001:db8:0:0:0:ff00:42:8329]:9042, this method
-     * removes port information and returns 27.0.0.1 or 2001:db8:0:0:0:ff00:42:8329.
+     * removes port information and returns 127.0.0.1 or 2001:db8:0:0:0:ff00:42:8329.
      * @param address
      * @return host address without port information
      */
@@ -15,6 +15,11 @@ public class RequestUtils
     {
         if (address.contains(":"))
         {
+            // just ipv6 host name present without port information
+            if (address.split(":").length > 2 && !address.startsWith("["))
+            {
+                return address;
+            }
             String host = address.substring(0, address.lastIndexOf(':'));
             // remove brackets from ipv6 addresses
             return host.startsWith("[") ? host.substring(1, host.length() - 1) : host;
