9d787dd04f974923de291f1e7e1c18e0598ba570
Francisco Guerrero <francisco.guerrero@apple.com>
2022-07-02 13:32:20 -0700

    Add javadocs for CassandraInputValidator

--- common/src/main/java/org/apache/cassandra/sidecar/common/utils/CassandraInputValidator.java
+++ common/src/main/java/org/apache/cassandra/sidecar/common/utils/CassandraInputValidator.java
@@ -18,7 +18,7 @@ import org.jetbrains.annotations.NotNull;
 @Singleton
 public class CassandraInputValidator
 {
-    private final Set<String> forbiddenDirs;
+    private final Set<String> forbiddenKeyspaceSet;
     private final Pattern patternWordChars;
     private final String componentRegex;
     private final String dbTocComponentRegex;
@@ -26,29 +26,57 @@ public class CassandraInputValidator
     @Inject
     public CassandraInputValidator(ValidationConfiguration validationConfiguration)
     {
-        forbiddenDirs = validationConfiguration.getForbiddenDirs();
+        forbiddenKeyspaceSet = validationConfiguration.getForbiddenDirs();
         patternWordChars = Pattern.compile(validationConfiguration.getCharsAllowedPattern());
         componentRegex = validationConfiguration.getComponentRegex();
         dbTocComponentRegex = validationConfiguration.getDbTocComponentRegex();
     }
 
-    public String validateKeyspaceName(final String keyspace)
+    /**
+     * Validates that the {@code keyspace} is not {@code null}, that it contains valid characters, and that it's
+     * not a forbidden keyspace.
+     *
+     * @param keyspace the name of the Cassandra keyspace to validate
+     * @return the validated {@code keyspace}
+     * @throws NullPointerException when the {@code keyspace} is {@code null}
+     * @throws HttpException        when the {@code keyspace} contains invalid characters in the name or when the
+     *                              keyspace is forbidden
+     */
+    public String validateKeyspaceName(@NotNull String keyspace)
     {
         Objects.requireNonNull(keyspace, "keyspace must not be null");
         validatePattern(keyspace, "keyspace");
-        if (forbiddenDirs.contains(keyspace))
+        if (forbiddenKeyspaceSet.contains(keyspace))
             throw new HttpException(HttpResponseStatus.FORBIDDEN.code(), "Forbidden keyspace: " + keyspace);
         return keyspace;
     }
 
-    public String validateTableName(final String tableName)
+    /**
+     * Validates that the {@code tableName} is not {@code null}, and it contains allowed character for Cassandra
+     * table names.
+     *
+     * @param tableName the name of the Cassandra table to validate
+     * @return the validated {@code tableName}
+     * @throws NullPointerException when the {@code tableName} is {@code null}
+     * @throws HttpException        when the {@code tableName} contains invalid characters in the name
+     */
+    public String validateTableName(@NotNull String tableName)
     {
         Objects.requireNonNull(tableName, "tableName must not be null");
         validatePattern(tableName, "table name");
         return tableName;
     }
 
-    public String validateSnapshotName(final String snapshotName)
+    /**
+     * Validates that the {@code snapshotName} is not {@code null}, and it contains allowed character for the
+     * Cassandra snapshot names.
+     *
+     * @param snapshotName the name of the Cassandra snapshot to validate
+     * @return the validated {@code snapshotName}
+     * @throws NullPointerException when the {@code snapshotName} is {@code null}
+     * @throws HttpException        when the {@code snapshotName} contains inalid characters in the name
+     */
+    public String validateSnapshotName(@NotNull String snapshotName)
     {
         Objects.requireNonNull(snapshotName, "snapshotName must not be null");
         //  most UNIX systems only disallow file separator and null characters for directory names
@@ -58,16 +86,44 @@ public class CassandraInputValidator
         return snapshotName;
     }
 
-    public String validateComponentName(String componentName)
+    /**
+     * Validates that the {@code componentName} is not {@code null}, and it contains allowed names for the
+     * Cassandra SSTable component.
+     *
+     * @param componentName the name of the SSTable component to validate
+     * @return the validated {@code componentName}
+     * @throws NullPointerException when the {@code componentName} is null
+     * @throws HttpException        when the {@code componentName} is not valid
+     */
+    public String validateComponentName(@NotNull String componentName)
     {
         return validateComponentNameByRegex(componentName, componentRegex);
     }
 
-    public String validateDbOrTOCComponentName(String componentName)
+    /**
+     * Validates that the {@code componentName} is not {@code null}, and it is a valid {@code *.db} or {@code *TOC.txt}
+     * Cassandra SSTable component.
+     *
+     * @param componentName the name of the SSTable component to validate
+     * @return the validated {@code componentName}
+     * @throws NullPointerException when the {@code componentName} is null
+     * @throws HttpException        when the {@code componentName} is not a valid {@code *.db} or {@code *TOC.txt} SSTable
+     *                              component
+     */
+    public String validateDbOrTOCComponentName(@NotNull String componentName)
     {
         return validateComponentNameByRegex(componentName, dbTocComponentRegex);
     }
 
+    /**
+     * Validates the {@code componentName} against the provided {@code regex}.
+     *
+     * @param componentName the name of the SSTable component
+     * @param regex         the regex for validation
+     * @return the validated {@code componentName}
+     * @throws NullPointerException when the {@code componentName} is null
+     * @throws HttpException        when the {@code componentName} does not match the provided regex
+     */
     @NotNull
     private String validateComponentNameByRegex(String componentName, String regex)
     {
@@ -78,6 +134,13 @@ public class CassandraInputValidator
         return componentName;
     }
 
+    /**
+     * Validates that the {@code input} matches the {@code patternWordChars}
+     *
+     * @param input the input
+     * @param name  a name for the exception
+     * @throws HttpException when the {@code input} does not match the pattern
+     */
     private void validatePattern(String input, String name)
     {
         final Matcher matcher = patternWordChars.matcher(input);
