dd948415b858dc7813b31b3131279670d0b00ad9
Saranya Krishnakumar <saranya_k@apple.com>
2024-02-09 15:36:55 -0800

    Add unit test

--- src/test/java/org/apache/cassandra/sidecar/server/ServerTest.java
+++ src/test/java/org/apache/cassandra/sidecar/server/ServerTest.java
@@ -33,6 +33,7 @@ import com.google.inject.Injector;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
+import io.vertx.core.net.TrafficShapingOptions;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.junit5.Checkpoint;
 import io.vertx.junit5.VertxExtension;
@@ -41,6 +42,8 @@ import io.vertx.junit5.VertxTestContext;
 import static org.apache.cassandra.sidecar.common.ResourceUtils.writeResourceToPath;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatException;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * Unit tests for {@link Server} lifecycle
@@ -134,6 +137,26 @@ class ServerTest
               .onSuccess(v -> assertThatException().isThrownBy(() -> server.start())
                                                    .withMessageContaining("Vert.x closed"))
               .onFailure(context::failNow);
+    }
+
+    @Test
+    @DisplayName("Update should fail with null options")
+    void updatingTrafficShapingOptionsWithNull(VertxTestContext context)
+    {
+        Checkpoint serverStarted = context.checkpoint();
+        Checkpoint waitUntilUpdate = context.checkpoint();
+
+        vertx.eventBus().localConsumer(SidecarServerEvents.ON_SERVER_START.address(), message -> serverStarted.flag());
+
+        server.start()
+              .compose(this::validateHealthEndpoint)
+              .onSuccess(v -> {
+                  assertThatIllegalArgumentException()
+                  .isThrownBy(() -> server.updateTrafficShapingOptions(null))
+                  .withMessage("Invalid null value passed for traffic shaping options update");
+                  waitUntilUpdate.flag();
+                  context.completeNow();
+              });
     }
 
     Future<String> validateHealthEndpoint(String deploymentId)
