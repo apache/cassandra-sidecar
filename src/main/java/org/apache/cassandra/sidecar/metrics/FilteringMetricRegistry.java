592ae906cd0bc5a562db334c022788e99662fad5
Saranya Krishnakumar <saranya_k@apple.com>
2024-04-10 11:28:53 -0700

    Unused import

--- src/main/java/org/apache/cassandra/sidecar/metrics/FilteringMetricRegistry.java
+++ src/main/java/org/apache/cassandra/sidecar/metrics/FilteringMetricRegistry.java
@@ -23,7 +23,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Function;
 import java.util.function.Predicate;
 
 import com.codahale.metrics.Counter;
