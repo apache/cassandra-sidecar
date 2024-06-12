9f58315d331196a7526fbbb221f983d3730850eb
raymondwelgosh <r_welgosh@apple.com>
2024-06-12 14:58:43 -0700

    Disabled Ulimit Test
    ulimit does not exist at the CI runtime so added a disabled annotation.

--- src/test/java/org/apache/cassandra/sidecar/UlimitTest.java
+++ src/test/java/org/apache/cassandra/sidecar/UlimitTest.java
@@ -22,6 +22,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -29,6 +30,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * Test to ensure that the max file descriptors is large enough to build Cassandra Sidecar
  */
+@Disabled("ulimit does not exist at the CI runtime")
 public class UlimitTest
 {
 
