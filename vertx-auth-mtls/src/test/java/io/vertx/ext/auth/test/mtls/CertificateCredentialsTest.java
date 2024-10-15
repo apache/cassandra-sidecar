db10659879af82d2da4ee220b9395da5aec65f67
Saranya Krishnakumar <saranya_k@apple.com>
2024-10-15 10:35:41 -0700

    Fix import order

--- vertx-auth-mtls/src/test/java/io/vertx/ext/auth/test/mtls/CertificateCredentialsTest.java
+++ vertx-auth-mtls/src/test/java/io/vertx/ext/auth/test/mtls/CertificateCredentialsTest.java
@@ -22,9 +22,9 @@ import java.security.cert.Certificate;
 import java.util.Collections;
 import java.util.List;
 
-import io.vertx.core.http.HttpConnection;
 import org.junit.jupiter.api.Test;
 
+import io.vertx.core.http.HttpConnection;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.ext.auth.authentication.CertificateCredentials;
 import io.vertx.ext.auth.authentication.CredentialValidationException;
@@ -54,6 +54,7 @@ public class CertificateCredentialsTest
         when(request.connection()).thenReturn(connection);
 
         assertThatThrownBy(() -> new CertificateCredentials(request).checkValid())
-                .isInstanceOf(CredentialValidationException.class);
+                .isInstanceOf(CredentialValidationException.class)
+                .hasMessage("Certificate Chain cannot be empty");
     }
 }
