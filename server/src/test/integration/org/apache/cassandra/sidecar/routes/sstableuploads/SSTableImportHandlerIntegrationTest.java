57d4da634b0da0524d24e89f489a7cde481154ab
Saranya Krishnakumar <saranya_k@apple.com>
2024-11-06 16:16:59 -0800

    checkstyle fix

--- server/src/test/integration/org/apache/cassandra/sidecar/routes/sstableuploads/SSTableImportHandlerIntegrationTest.java
+++ server/src/test/integration/org/apache/cassandra/sidecar/routes/sstableuploads/SSTableImportHandlerIntegrationTest.java
@@ -19,7 +19,6 @@
 package org.apache.cassandra.sidecar.routes.sstableuploads;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
