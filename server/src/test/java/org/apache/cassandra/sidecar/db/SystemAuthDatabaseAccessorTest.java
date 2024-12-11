6b1c157cd7a0d8f228e652aebc85f54509525593
Yifan Cai <ycai@apache.org>
2024-12-11 13:15:25 -0800

    fix test

--- server/src/test/java/org/apache/cassandra/sidecar/db/SystemAuthDatabaseAccessorTest.java
+++ server/src/test/java/org/apache/cassandra/sidecar/db/SystemAuthDatabaseAccessorTest.java
@@ -41,9 +41,9 @@ class SystemAuthDatabaseAccessorTest
                                                                                                mockCqlSessionProvider);
         assertThatThrownBy(()  -> systemAuthDatabaseAccessor.findRoleFromIdentity("spiffe://cassandra/sidecar/test"))
         .isInstanceOf(SchemaUnavailableException.class)
-        .hasMessage("SystemAuthSchema was not prepared, values cannot be retrieved from table");
+        .hasMessage("Table system_auth/identity_to_role does not exist");
         assertThatThrownBy(systemAuthDatabaseAccessor::findAllIdentityToRoles)
         .isInstanceOf(SchemaUnavailableException.class)
-        .hasMessage("SystemAuthSchema was not prepared, values cannot be retrieved from table");
+        .hasMessage("Table system_auth/identity_to_role does not exist");
     }
 }
