71aeed8b1b9a5c0cb4ff95d8437d9279786f2a85
Bernardo Botella Corbi <contacto@bernardobotella.com>
2025-02-12 16:11:18 -0800

    Fix licenses

--- client/src/testFixtures/java/org/apache/cassandra/sidecar/client/SidecarClientTest.java
+++ client/src/testFixtures/java/org/apache/cassandra/sidecar/client/SidecarClientTest.java
@@ -60,7 +60,6 @@ import okio.Buffer;
 import okio.Okio;
 import org.apache.cassandra.sidecar.client.exception.RetriesExhaustedException;
 import org.apache.cassandra.sidecar.client.request.RequestExecutorTest;
-import org.apache.cassandra.sidecar.client.retry.NoRetryPolicy;
 import org.apache.cassandra.sidecar.client.retry.RetryAction;
 import org.apache.cassandra.sidecar.client.retry.RetryPolicy;
 import org.apache.cassandra.sidecar.common.ApiEndpointsV1;
@@ -197,12 +196,12 @@ abstract class SidecarClientTest
         servers.get(1).enqueue(responseInstance1);
         servers.get(2).enqueue(responseInstance2);
 
-        HealthResponse response1 = client.sidecarInstanceHealth(instances.get(1), new NoRetryPolicy()).get(30, TimeUnit.SECONDS);
+        HealthResponse response1 = client.sidecarHealth().get(30, TimeUnit.SECONDS);
         assertThat(response1).isNotNull();
         assertThat(response1.status()).isEqualToIgnoringCase("OK");
         assertThat(response1.isOk()).isTrue();
 
-        assertThatThrownBy(() -> client.sidecarInstanceHealth(instances.get(2), new NoRetryPolicy()).get(30, TimeUnit.SECONDS))
+        assertThatThrownBy(() -> client.sidecarHealth())
                 .isInstanceOf(ExecutionException.class)
                 .hasCauseInstanceOf(RetriesExhaustedException.class);
 
