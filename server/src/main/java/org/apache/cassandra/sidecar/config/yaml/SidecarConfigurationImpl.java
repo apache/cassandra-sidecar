1aa6339df17159044077b32796eddd53402153f5
anderoo <andrew.johnson@reddit.com>
2025-06-19 19:22:05 +0100

    Shorten class with import

--- server/src/main/java/org/apache/cassandra/sidecar/config/yaml/SidecarConfigurationImpl.java
+++ server/src/main/java/org/apache/cassandra/sidecar/config/yaml/SidecarConfigurationImpl.java
@@ -38,6 +38,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.apache.cassandra.sidecar.common.DataObjectBuilder;
+import org.apache.cassandra.sidecar.common.server.dns.DnsResolver;
 import org.apache.cassandra.sidecar.common.server.dns.DnsResolvers;
 import org.apache.cassandra.sidecar.common.server.utils.MillisecondBoundConfiguration;
 import org.apache.cassandra.sidecar.config.AccessControlConfiguration;
@@ -386,8 +387,7 @@ public class SidecarConfigurationImpl implements SidecarConfiguration
                  });
 
         // Manually add type mapping for DnsResolver
-        module.addAbstractTypeMapping(org.apache.cassandra.sidecar.common.server.dns.DnsResolver.class,
-                                      DnsResolvers.class);
+        module.addAbstractTypeMapping(DnsResolver.class, DnsResolvers.class);
 
         Set<Class> unimplemented = Sets.difference(declared, implemented);
         if (!unimplemented.isEmpty())
