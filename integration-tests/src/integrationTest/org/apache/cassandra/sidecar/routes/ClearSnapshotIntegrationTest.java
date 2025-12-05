e2be4b20fd27130d99008aa34ed84c520dc96157
Shruti Sekaran <s_sekaran@apple.com>
2025-12-04 16:36:32 -0800

    Fix and tests 2

--- integration-tests/src/integrationTest/org/apache/cassandra/sidecar/routes/ClearSnapshotIntegrationTest.java
+++ integration-tests/src/integrationTest/org/apache/cassandra/sidecar/routes/ClearSnapshotIntegrationTest.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.cassandra.sidecar.routes;
 
 import java.nio.file.Path;
@@ -73,8 +92,8 @@ public class ClearSnapshotIntegrationTest extends SharedClusterSidecarIntegratio
 
         // Create the snapshot
         HttpResponse<Buffer> createResponse = getBlocking(
-            trustedClient().put(serverWrapper.serverPort, "127.0.0.1", testRoute)
-                          .send());
+        trustedClient().put(serverWrapper.serverPort, "127.0.0.1", testRoute)
+                       .send());
         assertThat(createResponse.statusCode()).isEqualTo(OK.code());
 
         // Verify snapshot files exist
@@ -83,8 +102,8 @@ public class ClearSnapshotIntegrationTest extends SharedClusterSidecarIntegratio
 
         // Delete the snapshot
         HttpResponse<Buffer> deleteResponse = getBlocking(
-            trustedClient().delete(serverWrapper.serverPort, "127.0.0.1", testRoute)
-                          .send());
+        trustedClient().delete(serverWrapper.serverPort, "127.0.0.1", testRoute)
+                       .send());
         assertThat(deleteResponse.statusCode()).isEqualTo(OK.code());
 
         // Verify snapshot files were deleted
