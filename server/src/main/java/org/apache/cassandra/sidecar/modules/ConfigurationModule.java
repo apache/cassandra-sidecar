c7cd3c70011f59e973738aa2b6667827c4f5fb6f
N V Harikrishna <n.v.harikrishna.apache@gmail.com>
2026-02-11 01:05:55 +0530

    Addressed review comments

--- server/src/main/java/org/apache/cassandra/sidecar/modules/ConfigurationModule.java
+++ server/src/main/java/org/apache/cassandra/sidecar/modules/ConfigurationModule.java
@@ -240,29 +240,22 @@ public class ConfigurationModule extends AbstractModule
                                                                          host,
                                                                          port,
                                                                          new InstanceHealthMetrics(instanceSpecificRegistry));
-        InstanceMetadataImpl.Builder builder = InstanceMetadataImpl.builder()
-                                                                    .id(cassandraInstance.id())
-                                                                    .host(host, dnsResolver)
-                                                                    .port(port);
-
-        // Only set storagePort if explicitly configured, otherwise use builder default
-        Integer configuredStoragePort = cassandraInstance.storagePort();
-        if (configuredStoragePort != null)
-        {
-            builder.storagePort(configuredStoragePort);
-        }
-
-        return builder.storageDir(cassandraInstance.storageDir())
-                      .dataDirs(cassandraInstance.dataDirs())
-                      .stagingDir(cassandraInstance.stagingDir())
-                      .cdcDir(cassandraInstance.cdcDir())
-                      .commitlogDir(cassandraInstance.commitlogDir())
-                      .hintsDir(cassandraInstance.hintsDir())
-                      .savedCachesDir(cassandraInstance.savedCachesDir())
-                      .localSystemDataFileDir(cassandraInstance.localSystemDataFileDir())
-                      .lifecycleOptions(cassandraInstance.lifecycleOptions())
-                      .delegate(delegate)
-                      .metricRegistry(instanceSpecificRegistry)
-                      .build();
+        return InstanceMetadataImpl.builder()
+                                   .id(cassandraInstance.id())
+                                   .host(host, dnsResolver)
+                                   .port(port)
+                                   .storagePort(cassandraInstance.storagePort())
+                                   .storageDir(cassandraInstance.storageDir())
+                                   .dataDirs(cassandraInstance.dataDirs())
+                                   .stagingDir(cassandraInstance.stagingDir())
+                                   .cdcDir(cassandraInstance.cdcDir())
+                                   .commitlogDir(cassandraInstance.commitlogDir())
+                                   .hintsDir(cassandraInstance.hintsDir())
+                                   .savedCachesDir(cassandraInstance.savedCachesDir())
+                                   .localSystemDataFileDir(cassandraInstance.localSystemDataFileDir())
+                                   .lifecycleOptions(cassandraInstance.lifecycleOptions())
+                                   .delegate(delegate)
+                                   .metricRegistry(instanceSpecificRegistry)
+                                   .build();
     }
 }
